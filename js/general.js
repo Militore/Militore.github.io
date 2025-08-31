3aa74699b14534c872a2f48a4e2f5057a339cf50
Militore <lordmilitore@gmail.com>
2025-08-31 11:30:04 +0300

    add change theme functionality

--- js/general.js
+++ js/general.js
@@ -34,3 +34,30 @@ $("#nav-hide").click(function(){
   }
   $("nav").slideToggle(400);
 });
+
+// Change site theme with jquery
+const defHeadText = $("body").css("--head-text");
+const defMainText = $("body").css("--main-text");
+
+var changeTheme = function(head, headSec, mainSec, main, headText, mainText){
+  $("body").get(0).style.setProperty("--head-color", head);
+  $("body").get(0).style.setProperty("--head-sec-color", headSec);
+  $("body").get(0).style.setProperty("--main-sec-color", mainSec);
+  $("body").get(0).style.setProperty("--main-color", main);
+  if(headText==null){headText = defHeadText;}
+  if(mainText==null){mainText = defMainText;}
+  $("body").get(0).style.setProperty("--head-text", headText)
+  $("body").get(0).style.setProperty("--main-text", mainText)
+}
+
+$("#change-theme").click(function(){
+  if($("#change-theme").val() == 1) {
+    changeTheme("#222831", "#393E46", "#948979", "#DFD0B8");
+  }
+  if($("#change-theme").val() == 2) {
+    changeTheme("#37353E", "#44444E", "#715A5A", "#D3DAD9");
+  }
+  if($("#change-theme").val() == 3) {
+    changeTheme("#D9C4B0", "#CFAB8D", "#BBDCE5", "#ECEEDF", "#000", "#000");
+  }
+});
